2f28ce5a0438ba70ab08c7f3b7ddc19fa6a87dfe
wangmin <wangmin0880@163.com>
2023-08-25 05:39:39 +0800

    Add one test function.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
+#include "math.h"
+#include "stdio.h"
+/*
 int add(int a, int b)
 {
     return a + b;
 }
-
+*/
+void testF()
+{
+    printf("F(x, y) = x + y when x = 5, y = 6, result : %d \n", math_add(5,6));
+    printf("F(x) = x^2 when x = 5, result : %d \n", math_square(5));
+    return;
+}
 int main()
 {
+    /*
     int a = 0;
     int b = 0;
-    int c = add(a, b);
-    return c;
+    int c = F(a, b);
+    */
+    testF();
+    return 0;
 }
