4ce83ebb75667b5eeaf4094f9b83663815d4c761
wangmin <wangmin0880@163.com>
2023-08-26 06:21:24 +0800

    Add area.h to topic calculate the circle area .

---
+++ area.h
@@ -0,0 +1,11 @@
+/*
+* We will focus on the area topic this part
+*                   ----wangmin0880@163.com
+*/
+
+/*
+* circle area
+* y = pi * r^2
+*/
+#define m_pi 3.1415926536
+#define m_circle_area(r) (m_pi * (r * r))
--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
-#include "math.h"
 #include "stdio.h"
-/*
-int add(int a, int b)
-{
-    return a + b;
-}
-*/
-void testF()
+
+#include "math.h"
+#include "area.h"
+
+void test_math()
 {
     printf("F(x, y) = x + y when x = 5, y = 6, result : %d \n", math_add(5,6));
     printf("F(x) = x^2 when x = 5, result : %d \n", math_square(5));
     return;
 }
+
+void test_area()
+{
+    printf("circle area S = pi * r^2 when r = 5, result : %f \n", m_circle_area(5));
+}
+
 int main()
 {
-    /*
-    int a = 0;
-    int b = 0;
-    int c = F(a, b);
-    */
-    testF();
+    //test_math();
+    test_area();
     return 0;
 }
